308f756edbeabb79fad46b57ac2db1b09689cf7b
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-10-28 16:09:22 +0000

    Update task definition with health checks (#53)
    * Add health check to task definition
    
    * Update cd workflow

--- terraform/modules/ecs/ecs.tf
+++ terraform/modules/ecs/ecs.tf
@@ -178,6 +178,13 @@ resource "aws_ecs_task_definition" "td" {
           value = "/app/.ipython"
         }
       ]
+      healthCheck = {
+        command     = ["CMD-SHELL", "curl -f http://localhost:8000 || exit 1"]
+        interval    = 30
+        timeout     = 5
+        retries     = 3
+        startPeriod = 60
+      }
       logConfiguration = {
         logDriver = "awslogs"
         options = {
