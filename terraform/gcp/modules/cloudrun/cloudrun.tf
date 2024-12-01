66c03aeafaeaaf3ec0bc623cab6cada89b57a4d7
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-12-01 16:58:51 +0000

    Fix env vars

--- terraform/gcp/modules/cloudrun/cloudrun.tf
+++ terraform/gcp/modules/cloudrun/cloudrun.tf
@@ -8,7 +8,7 @@ resource "google_cloud_run_service" "postgres" {
       containers {
         image = "postgres:16"
         ports {
-          container_port = 8080
+          container_port = tostring(var.db_port)
         }
         env {
           name  = "POSTGRES_USER"
@@ -21,10 +21,6 @@ resource "google_cloud_run_service" "postgres" {
         env {
           name  = "POSTGRES_DB"
           value = var.postgres_db
-        }
-        env {
-          name  = "POSTGRES_PORT"
-          value = tostring(var.db_port)
         }
       }
     }
@@ -53,7 +49,7 @@ resource "google_cloud_run_service" "django" {
       containers {
         image = "gcr.io/google-samples/hello-app:1.0"
         ports {
-          container_port = 8080
+          container_port = tostring(var.db_port)
         }
 
         env {
