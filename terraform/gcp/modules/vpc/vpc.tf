5c15dd2709b3e753a81bd842be5439d5a20c8bca
Shane O'Hanlon <70384009+shaneoh10@users.noreply.github.com>
2024-12-02 12:53:01 +0000

    Update VPC

--- terraform/gcp/modules/vpc/vpc.tf
+++ terraform/gcp/modules/vpc/vpc.tf
@@ -31,7 +31,7 @@ resource "google_compute_subnetwork" "private_sn1" {
   region        = var.region
   network       = google_compute_network.vpc.id
 
-  private_ip_google_access = false
+  private_ip_google_access = true
 }
 
 resource "google_compute_subnetwork" "private_sn2" {
@@ -40,10 +40,10 @@ resource "google_compute_subnetwork" "private_sn2" {
   region        = var.region
   network       = google_compute_network.vpc.id
 
-  private_ip_google_access = false
+  private_ip_google_access = true
 }
 
-# Cloud Router (equivalent to NAT Gateway functionality)
+# Cloud Router
 resource "google_compute_router" "router" {
   name    = "${var.project_id}-router"
   region  = var.region
@@ -59,8 +59,7 @@ resource "google_compute_router_nat" "nat" {
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
 
-# Firewall Rules (replacing Security Groups)
-# ALB Firewall Rule
+# Firewall Rules
 resource "google_compute_firewall" "alb_fw" {
   name    = "${var.project_id}-alb-fw"
   network = google_compute_network.vpc.id
@@ -71,46 +70,3 @@ resource "google_compute_firewall" "alb_fw" {
   }
 
   source_ranges = ["0.0.0.0/0"]
-  target_tags   = ["alb"]
-}
-
-# App Firewall Rule (incoming traffic to app)
-resource "google_compute_firewall" "app_fw" {
-  name    = "${var.project_id}-app-fw"
-  network = google_compute_network.vpc.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["8000"]
-  }
-
-  source_tags = ["alb"]
-  target_tags = ["app"]
-}
-
-# Database Firewall Rule
-resource "google_compute_firewall" "db_fw" {
-  name    = "${var.project_id}-db-fw"
-  network = google_compute_network.vpc.id
-
-  allow {
-    protocol = "tcp"
-    ports    = ["5432"]
-  }
-
-  source_tags = ["app"]
-  target_tags = ["db"]
-}
-
-# Egress Firewall Rule (Allow all outbound)
-resource "google_compute_firewall" "egress_fw" {
-  name      = "${var.project_id}-egress-fw"
-  network   = google_compute_network.vpc.id
-  direction = "EGRESS"
-
-  allow {
-    protocol = "all"
-  }
-
-  destination_ranges = ["0.0.0.0/0"]
-}
